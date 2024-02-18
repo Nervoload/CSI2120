e29f96f6333bc464d9cc30cbda1817043f79f13f
Felipe Garcia Affonso <114444325+Felipe047@users.noreply.github.com>
2024-02-18 16:43:36 -0500

    Update SimilaritySearch.java

--- SimilaritySearch.java
+++ SimilaritySearch.java
@@ -1,3 +1,31 @@
-public class SimilaritySearch {
+/*
+ * Felipe Garcia Affonso 300290722
+ * John Surette 300307306
+ */
 
+import java.util.PriorityQueue;
+
+public class SimilaritySearch {
+    public static void main(String[] args) {
+        String imagePath = "./queryImages/" + args[0];
+        String dataSetPath = "./" + args[1];
+        PriorityQueue<ImageDistance> minHeap = new PriorityQueue<ImageDistance>();
+
+        ColorImage image = new ColorImage(imagePath);
+        ColorHistogram imageHistogram = new ColorHistogram(3);
+
+        imageHistogram.setImage(image);
+
+        for (int i = 0; i < 5000; i++) {
+            ColorHistogram h = new ColorHistogram(dataSetPath + "/" + i + ".jpg.txt");
+
+            if (h.getHistogram() != null) {
+                minHeap.add(new ImageDistance(i + ".jpg", imageHistogram.compare(h)));
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(minHeap.poll().toString());
+        }
+    }
 }
